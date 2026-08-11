7e21bf62b034cd7a060983c9f2a5bcb928d0598b
Arman Dubey <Dubeyarman834@gmail.com>
2026-08-11 14:44:53 +0530

    commit

---
+++ basics]/datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+//alert(3+3)
+console.log(3+3);console.log("jui")
+let name = "huiui"
+let age = 18
+let islogin = false
+let state = null
+console.log(typeof age)
+console.table([name, age, islogin, state]);
---
+++ basics]/vairables.js
@@ -0,0 +1,8 @@
+const acc_id = 14541
+let acc_eml = "ar@gmail.com"
+var acc_pass= "123456"
+acc_city = "Jayork"
+let acc_state ;
+
+//acc_id = 2
+console.table([acc_id, acc_eml, acc_pass, acc_city,acc_state]);
