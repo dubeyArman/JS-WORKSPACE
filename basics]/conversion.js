d268799cca8148579f6c2b58a3cdcefb061adfa7
Arman Dubey <Dubeyarman834@gmail.com>
2026-08-11 18:48:43 +0530

    commit

---
+++ basics]/conversion.js
@@ -0,0 +1,12 @@
+let score = 33 
+console.log(typeof score);
+console.log(typeof (score));
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber);
+let isloggedin = 0
+let booleanisloggedin = Boolean(isloggedin);
+console.log(booleanisloggedin);
+let somenumer=33
+let stringno=String(somenumer)
+console.log(stringno)
+console.log(typeof stringno)
