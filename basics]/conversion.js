5f2bb1cb34a4e76b00f99d489021aa59f863a8cd
Arman Dubey <Dubeyarman834@gmail.com>
2026-08-14 19:54:53 +0530

    commit

--- basics]/conversion.js
+++ basics]/conversion.js
@@ -10,3 +10,12 @@ let somenumer=33
 let stringno=String(somenumer)
 console.log(stringno)
 console.log(typeof stringno)
+let value = 29382
+let negvalue = -value
+console.log(negvalue)
+let num1,num2,num3
+num1=num2=num3=2+32
+console.log(num1,num2,num3)
+let gameCount =100
+++gameCount;
+console.log(gameCount)
